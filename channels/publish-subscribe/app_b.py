5fdc84b2a51569b3493cd9fe4a1417a85102d27d
sguzunov <sguzunov@gmail.com>
2020-08-10 14:32:44 +0300

    Add examples for channels

---
+++ channels/publish-subscribe/app_b.py
@@ -0,0 +1,70 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_html_components as html
+import dash_core_components as dcc
+import time
+import dash_glue
+from run import server
+
+app = dash.Dash(__name__, server=server, routes_pathname_prefix='/app-b/')
+
+# Dropdown option, that will be used to leave the current channel.
+no_channel = { "label": "No Channel", "value": "" }
+
+# To use Channels API, we need to enable channels in the config.
+app.layout = dash_glue.glue42(id='glue42', config={'channels': True}, children = [
+    # Instantialing the channels component.
+    dash_glue.channels(id="glue42-channels"),
+
+    html.H3('Application B (Publish and Subscribe Channels)'),
+    html.Hr(),
+
+    html.Div(children=[
+        html.Label('Select Channel: '),
+        dcc.Dropdown(id='channels-list'),
+        html.Button(id="publish-data", children="Publish Data")
+    ]),
+])
+
+def channels_to_dpd_options(channels_info):
+    if channels_info is not None:
+        options = map(lambda channel: { "label": channel["name"], "value": channel["name"] }, channels_info)
+        return [no_channel] + list(options)
+
+    return [no_channel]
+
+# Discovering the list of all channels.
+@app.callback(Output('channels-list', 'options'), [Input('glue42-channels', 'channelsInfo')])
+def update_channels_list(channels_info):
+    return channels_to_dpd_options(channels_info)
+
+# Logic whether to join a channel or leave the current one.
+@app.callback(Output('glue42-channels', 'change'), [Input('channels-list', 'value')])
+def change_channel(channel_name):
+    if channel_name is not None:
+        if channel_name == no_channel["value"]:
+            return { 'action': 'leave' }
+        else:
+            return { 'action': 'join', 'channelName': channel_name }
+
+@app.callback(Output('channels-list', 'style'), [Input('glue42-channels', 'incoming')])
+def channel_changed(value):
+    if value is not None and value["channel"]:
+        channel = value["channel"]
+        color = channel["meta"]["color"]
+        
+        return { "backgroundColor": color }
+
+@app.callback(Output('glue42-channels', 'outgoing'), [Input('publish-data', 'n_clicks')])
+def publish_data(n_clicks):
+    if n_clicks != 0:        
+        now = time.time()
+
+        return { 
+            "data": {
+                "time": now
+            } 
+        }
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port='8050')
---
+++ channels/publish-subscribe/run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_from_directory
+import os.path as path
+
+server = Flask(__name__, template_folder = ".", static_url_path = "/")
+
+import app_a
+import app_b
+
+@server.route("/")
+def index():
+    return render_template('index.html')
+
+@server.route("/glue/<path:filename>")
+def glue_configs(filename):
+    d = path.abspath(path.join(__file__ ,"../../../"))
+    glue_dir = path.join(d, 'static\glue')
+
+    return send_from_directory(glue_dir, filename)
