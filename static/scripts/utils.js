19486b18d79cc3e26f24bb37ca00a5336e6054c0
sguzunov <sguzunov@gmail.com>
2021-01-19 17:29:28 +0200

    Support GC v2 in register-invoke-method

---
+++ static/scripts/utils.js
@@ -0,0 +1,30 @@
+(function (window) {
+    function displayApplicationsList(applications) {
+        (applications || []).forEach(app => {
+            const appName = app.name;
+            const displayName = app.title;
+
+            const linkEl = document.createElement('a');
+            linkEl.innerText = `${displayName}`;
+            linkEl.href = `#`;
+
+            linkEl.addEventListener('click', function () {
+                startApplication(appName)
+                return false;
+            });
+
+            const itemEl = document.createElement('li');
+            itemEl.appendChild(linkEl);
+
+            document.getElementById('applications-list').appendChild(itemEl);
+        });
+    }
+
+    function toggleGlueAvailable() {
+        document.getElementById("glueImg").src = "assets/connected.svg";
+        document.getElementById("glueSpan").textContent = "Main App (Web Platform) Connected";
+    };
+
+    window.toggleGlueAvailable = toggleGlueAvailable;
+    window.displayApplicationsList = displayApplicationsList;
+})(window || {});
